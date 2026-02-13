22d8d76492011f2d81c5aaada7676592b02aae28
cmil394 <cmil394@aucklanduni.ac.nz>
2026-02-13 17:03:01 +1300

    Frontend setup

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div className="Home">
+      <h1>Fulfillment Plus Time Software</h1>
+      <p>Welcome to the Fulfillment Plus Time Software!</p>
+    </div>
+  );
+}
+
+export default Home;
