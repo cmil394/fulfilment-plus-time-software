f900e379dd2778a4b9cf505aa0c8e478c585e5a0
cmil394 <carsonkmills09@gmail.com>
2026-04-18 19:20:15 +1200

    Users logged out when routing to kiosk page

--- frontend/src/pages/Kiosk/Kiosk.tsx
+++ frontend/src/pages/Kiosk/Kiosk.tsx
@@ -1,6 +1,16 @@
+import { useEffect } from "react";
 import Navbar from "../../components/Navbar/Navbar";
+import { useAuth } from "../../context/AuthContext";
 
 export default function Kiosk() {
+  const { logout, user } = useAuth();
+
+  useEffect(() => {
+    if (user) {
+      logout();
+    }
+  }, []);
+
   return (
     <div>
       <Navbar />
