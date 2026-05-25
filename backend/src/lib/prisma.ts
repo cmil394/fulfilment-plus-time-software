bce5034345414c6586c675f91f405f41d3b84b5a
cmil394 <carsonkmills09@gmail.com>
2026-05-25 20:27:24 +1200

    Add index on TimeEntry for userId and startTime

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -13,6 +13,8 @@ let prisma: PrismaClient;
 if (!globalForPrisma.prisma) {
   const adapter = new PrismaPg({
     connectionString: process.env.DATABASE_URL!,
+    min: 2,
+    max: 10,
   });
 
   globalForPrisma.prisma = new PrismaClient({
